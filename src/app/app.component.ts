75f5db53b63047e1f3ce437c96538e5e11060a54
Jhair L. Aorus <jlescanog@outlook.com>
2024-05-26 22:55:57 -0500

    CRUD ver 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+
+
+interface personModel {
+  nombres:string;
+  apellidos:string;
+  fechaNacimiento:Date;
+  direccion:string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    //BrowserModule,
+    RouterOutlet,
+    FormsModule,
+    CommonModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '02Crud';
+  
+  nombres: string = "Jhair Benjamin";
+  apellidos: string = "Lescano Guevara";
+
+  personas: personModel[] = [
+    {
+      nombres: "Jhair",
+      apellidos:"Lescano Guevara",
+      fechaNacimiento: new Date,
+      direccion: "Mi dirección"
+    },
+    {
+      nombres: "Luis",
+      apellidos:"Lujan Escobar",
+      fechaNacimiento: new Date,
+      direccion: "Mi dirección"
+    },
+    {
+      nombres: "Jhonatan",
+      apellidos:"Lescano Guevara",
+      fechaNacimiento: new Date,
+      direccion: "Mi dirección"
+    },
+    {
+      nombres: "Maria",
+      apellidos:"Bisso chavez",
+      fechaNacimiento: new Date,
+      direccion: "Mi dirección"
+    },
+    {
+      nombres: "Raul",
+      apellidos:"Puertar Chio",
+      fechaNacimiento: new Date,
+      direccion: "Mi dirección"
+    }
+
+  ]
+
 }
